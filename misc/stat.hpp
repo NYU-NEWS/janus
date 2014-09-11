28e0890545f8fbd78b39e5c6511efc9b79f9163d
msmummy <msmummy@gmail.com>
2014-09-11 11:25:35 +0000

    stat

--- misc/stat.hpp
+++ misc/stat.hpp
@@ -7,19 +7,25 @@ public:
     int64_t n_stat_;
     int64_t sum_;
     int64_t avg_;
+//    int64_t max_;
+//    int64_t min_;
 
-    AvgStat(): n_stat_(0), sum_(0), avg_(0) {}
+    AvgStat(): n_stat_(0), sum_(0), avg_(0), max_(0), min_(0) {}
     
-    void sample(int64_t s) {
+    void sample(int64_t s = 1) {
         ++n_stat_;
         sum_ += s;
         avg_ = sum_ / n_stat_;
+//        max_ = s > max_ ? s : max_;
+//        min_ = s < min_ ? s : min_;
     }
 
     void clear() {
         n_stat_ = 0;
         sum_ = 0;
         avg_ = 0;
+        max_ = 0;
+        min_ = 0;
     }
 
     AvgStat reset() {
@@ -32,6 +38,9 @@ public:
         return *this;
     }
 
+    int64_t avg() {
+        return avg_;
+    }
 };
 
 } // namespace rrr
