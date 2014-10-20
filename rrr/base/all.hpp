a017c5b1e9a53f3ded7aae66e24ce2f0baacc230
msmummy <msmummy@gmail.com>
2014-10-20 10:39:09 +0800

    update rrr as submodule

--- rrr/base/all.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "basetypes.hpp"
-#include "debugging.hpp"
-#include "logging.hpp"
-#include "misc.hpp"
-#include "strop.hpp"
-#include "threading.hpp"
-#include "unittest.hpp"
--- rrr/utils/mlock.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void mlock_init();
-
-void m_lock(char *name);
-
-void m_unlock(char *name);
-
-pthread_mutex_t* m_getlock(char* name);
