ab9e527bd292f8acf4e3904e90635d3226a2058a
msmummy <msmummy@gmail.com>
2015-09-27 06:18:47 +0000

    add missing file

---
+++ bench/tpca/chopper.cc
@@ -0,0 +1,43 @@
+
+#include "./bench/tpca/piece.h"
+#include "./bench/tpca/chopper.h"
+
+namespace rococo {
+
+void TpcaPaymentChopper::init(TxnRequest &req) {
+  verify(req.txn_type_ == TPCA_PAYMENT);
+  txn_type_ = TPCA_PAYMENT;
+
+  Value& cus = req.input_[0];
+  Value& tel = req.input_[1];
+  Value& bra = req.input_[2];
+  //Value& inc = req.input_[3];
+
+  inputs_.clear();
+  inputs_.push_back({cus/*, inc*/});
+  inputs_.push_back({tel/*, inc*/});
+  inputs_.push_back({bra/*, inc*/});
+
+  output_size_.assign({0, 0, 0});
+
+  this->p_types_ = {TPCA_PAYMENT_1, TPCA_PAYMENT_2, TPCA_PAYMENT_3};
+
+  // get sharding info
+  sharding_ = {0, 0, 0};
+  Sharding::get_site_id(TPCA_CUSTOMER, cus, sharding_[0]);
+  Sharding::get_site_id(TPCA_TELLER, tel, sharding_[1]);
+  Sharding::get_site_id(TPCA_BRANCH, bra, sharding_[2]);
+
+  // all pieces are ready
+  n_pieces_ = 3;
+  callback_ = req.callback_;
+  max_try_ = req.n_try_;
+  n_try_ = 1;
+
+
+  status_ = {0, 0, 0};
+  commit_.store(true);
+
+}
+
+} // namespace rococo
