0a58530775487873d231b8e5e2871d0154239272
msmummy <msmummy@gmail.com>
2015-12-03 01:31:42 +0000

    add missing file

---
+++ deptran/occ/coord.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../three_phase/coord.h"
+
+namespace rococo {
+
+class OCCCoord: public ThreePhaseCoord {
+  using ThreePhaseCoord::ThreePhaseCoord;
+};
+
+} //namespace rococo
