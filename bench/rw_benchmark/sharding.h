0a3653f67538a858791d0599127852b2992233d2
msmummy <msmummy@gmail.com>
2016-01-07 21:42:14 -0500

    add missing file

---
+++ bench/rw_benchmark/sharding.h
@@ -0,0 +1,10 @@
+//
+// Created by lamont on 1/4/16.
+//
+#pragma once
+#include <bench/tpcc_real_dist/sharding.h>
+
+namespace rococo {
+  class RWBenchmarkSharding : public TPCCDSharding {
+  };
+}
