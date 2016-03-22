a8eea8b70911067bde13913a389b3fc4e768328a
lamont <lamont.nelson@nyu.edu>
2016-03-22 17:37:58 -0400

    missing comma

--- pylib/ec2.py
+++ pylib/ec2.py
@@ -29,12 +29,12 @@ EC2_REGIONS = {
     },
     
     # these regions are not used
-    'ap-southeast-1': {}
-    'ap-southeast-2': {}
-    'ap-northeast-1': {}
-    'us-east-1': {}
-    'sa-east-1': {}
-    'us-west-1': {}
+    'ap-southeast-1': {},
+    'ap-southeast-2': {},
+    'ap-northeast-1': {},
+    'us-east-1': {},
+    'sa-east-1': {},
+    'us-west-1': {},
 }
 
 INSTANCE_TYPE = 't2.micro'
