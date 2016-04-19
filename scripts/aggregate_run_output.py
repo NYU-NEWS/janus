c5ef55771ab56e538cfca4cad7767f79bab33c46
lamont <lamont.nelson@nyu.edu>
2016-04-19 02:45:21 -0400

    script to collect the yml data into csv files for graphs

---
+++ scripts/aggregate_run_output.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import os.path
+import sys
+import traceback
+import yaml
+import argparse
+from itertools import groupby
+
+ARGS = None
+labels = ['txn_name', 'cc', 'ab', 'benchmark', 'duration', 'clients',
+          'tps', 'attempts', 'commits', 'start_cnt', 'total_cnt']
+lat_labels = None
+
+def parse_args():
+    global ARGS
+    parser = argparse.ArgumentParser(description="aggregate yml data")
+    parser.add_argument('input_files', nargs='+')
+    parser.add_argument('--force', '-f', dest="force", action="store_const",
+                        const=True, default=False)
+    ARGS = parser.parse_args(sys.argv[1:])
+
+def read_files():
+    results=[]
+    for fn in ARGS.input_files:
+        try:
+            with open(fn, 'r') as f:
+                results.append(yaml.load(f)) 
+        except:
+            traceback.print_exc()
+    return results
+
+def extract_data(txn_data, file_data):
+    for experiment in file_data:
+        for txn in experiment.keys():
+            row = []
+            etxn = experiment[txn]
+            for l in labels:
+                row.append(etxn[l])
+            latencies = etxn['att_latency']
+            global lat_labels
+            lat_labels = sorted(latencies.keys())
+            for l in lat_labels:
+                row.append(latencies[l])
+            if txn not in txn_data:
+                txn_data[txn] = []
+            a = txn_data[txn]
+            a.append(row)
+
+def row_key_clients(r):
+    return r[0:4]+[r[5]]
+def row_key(r):
+    return r[0:4]
+
+def output_data_file(key, group):
+    fn='_'.join(key) + ".csv"
+    print("generate csv: {}".format(fn))
+    if os.path.exists(fn) and not ARGS.force:
+        raise RuntimeError("file already exists: {}".format(fn))
+    with open(fn, 'w') as f:
+        f.write("# ")
+        f.write(", ".join(labels + lat_labels))
+        f.write("\n")
+        for row in group:
+            srow = [str(x) for x in row]
+            f.write(", ".join(srow)) 
+            f.write("\n") 
+
+def main():
+    parse_args()
+    file_data = read_files()
+    txn_data = {}
+    extract_data(txn_data, file_data)
+    for txn, rows in txn_data.iteritems():
+        rows.sort(key=row_key_clients)
+        for k, g in groupby(rows, row_key):
+            output_data_file(k, g)
+
+if __name__ == "__main__":
+    main()
