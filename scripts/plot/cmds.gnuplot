c33c3ade0f69323503adc96f5464203885b627fb
lamont <lamont.nelson@nyu.edu>
2016-04-26 20:36:25 -0400

    graph generation

---
+++ scripts/plot/cmds.gnuplot
@@ -0,0 +1,35 @@
+# paramaters:
+# x_label, y_label, graph_title, input_files, colors, output_file, terminal_type, 
+# x_tics, y_tics, x_range, y_range, x_col, y_col, key_locations, using_cmd
+
+if (!exists("colors"))        colors = "#AA1111 #11AA11 #1111AA"
+if (!exists("graph_title"))   graph_title = ""
+if (!exists("output_file"))   output_file = "out.png"
+if (!exists("terminal_type")) terminal_type = "pngcairo"
+if (!exists("x_label"))       x_label = ""
+if (!exists("y_label"))       y_label = ""
+if (!exists("using_cmd"))     using_cmd = "6:7"
+if (!exists("x_range"))       set autoscale x
+if (!exists("y_range"))       set autoscale y
+if (!exists("key_location"))  key_location = "top right"
+if (!exists("style"))         style = ""
+
+symbols = ""
+set macros
+set terminal terminal_type
+set output output_file
+set datafile separator ','
+set title graph_title
+set xlabel x_label
+set ylabel y_label
+set key @key_location
+if (exists("x_tics")) set xtics @x_tics
+if (exists("y_tics")) set ytics @y_tics
+if (exists("x_range")) set xrange @x_range
+if (exists("y_range")) set yrange @y_range
+if (exists("log_scale")) set logscale @log_scale
+
+plot for [i=1:words(input_files)] \
+word(input_files, i) using @using_cmd \
+title column(1) \
+with linespoints pt (i+2 % 50) ps 2 lt rgb word(colors, i)
---
+++ scripts/plot/do_plot.py
@@ -0,0 +1,126 @@
+import argparse
+import logging
+import yaml
+import copy
+import subprocess
+from string import Template
+
+default_gnuplot_exe="/usr/local/bin/gnuplot5"
+default_gnuplot_cmds="./cmds.gnuplot"
+
+args = None
+
+# filters to get the data
+def bench(k,*args):
+    return k[3] == args[0]
+
+def txn(k, *args):
+    return k[0] == args[0]
+############################
+
+
+def parse_args():
+    global args
+    parser = argparse.ArgumentParser(description='generate graphs.')
+    parser.add_argument('-d', '--data', 
+                        dest="data_files",
+                        metavar='FILE', 
+                        type=str, 
+                        nargs='+',
+                        help='the data files')
+    parser.add_argument('-g', '--graph-config', 
+                        metavar='FILE',
+                        dest="graphs",
+                        type=str, 
+                        nargs='+',
+                        help='the data files')
+    parser.add_argument('-gexe', '--gnuplot-exe',
+                        metavar='FILE',
+                        dest="gnuplot_exe",
+                        type=str, 
+                        default=default_gnuplot_exe,
+                        help='gnuplot executable')
+    parser.add_argument('-gcmds', '--gnuplot-cmds',
+                        metavar='FILE',
+                        dest="gnuplot_cmds",
+                        type=str,
+                        default=default_gnuplot_cmds,
+                        help='gnuplot commands')
+    args = parser.parse_args()
+
+
+def classify_data():
+    global args
+    output = {}
+    for fn in args.data_files:
+        lines = open(fn,'r').readlines()
+        for l in lines:
+            if l.strip()[0] != '#':
+                key = tuple(l.split(',')[0:4]) # key is txn,cc,ab,bench
+                key = tuple([k.strip() for k in key])
+                if key not in output:
+                    output[key] = fn
+                else:
+                    logging.warning("duplicate key found for {} - {}; previous {}".format(key, fn, output[key]))
+                break
+    return output
+
+
+
+def get_data(data_classes, filters):
+    output = []
+    for key, fn in data_classes.iteritems():
+        res = []
+        for f in filters:
+            #print(f[0], f[1:], key[3], f[0](key,*f[1:]))
+            res.append( f[0](key,*f[1:]) )
+        if all(res):
+            output.append(fn)
+    return output
+
+
+def run_gnuplot(settings, data_files):
+    global args
+    
+    if (data_files is None) or (len(data_files)==0):
+        return
+
+    cmd = [args.gnuplot_exe]
+    for k,v in settings.iteritems():
+        cmd.append('-e')
+        cmd.append("{k}='{v}';".format(k=k, v=v))
+
+    cmd.append('-e')
+    cmd.append("input_files='{}'".format(' '.join(data_files)))
+
+    cmd.append(args.gnuplot_cmds)
+    logging.info("running: {}".format(' '.join(['"{}"'.format(c) for c in cmd])))
+    res = subprocess.call(cmd)
+
+    if res != 0:
+        logging.error("Error {} running gnuplot!".format(res))
+
+
+def generate_graph(config, txn_types, data_classes):
+    for txn_name in txn_types:
+        data_files = get_data(data_classes, [ [bench, config['bench']], [txn, txn_name]])
+        gnuplot_settings = copy.copy(config['graph'])
+        gnuplot_settings['output_file'] = Template(gnuplot_settings['output_file']).substitute(txn=txn_name)
+        run_gnuplot(gnuplot_settings, data_files)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    global args
+    parse_args()
+    data_classes = classify_data()
+    txn_types = set([k[0] for k in data_classes.keys()])
+    for fn in args.graphs:
+        with open(fn, 'r') as f:
+            config = yaml.load(f)
+            generate_graph(config, txn_types, data_classes)
+
+
+if __name__ == "__main__":
+    main()
+
