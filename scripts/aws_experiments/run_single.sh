3a4f5cf4ac7473f6777d7c8f05dd2b18e30a0d13
lamont <lamont.nelson@nyu.edu>
2016-04-27 15:08:01 -0400

    missing file

--- scripts/aws_experiments/run_single.sh
+++ scripts/aws_experiments/run_single.sh
@@ -1,1 +1,1 @@
-./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_10.yml -cc config/tpca.yml -cc config/tapir.yml -b tpca -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -s 1 -r 3 -d 90 single_datacenter_fixed
+./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_10.yml -cc config/tpca_fixed.yml -cc config/tapir.yml -b tpca -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -s 1 -r 3 -d 90 single_datacenter_fixed
