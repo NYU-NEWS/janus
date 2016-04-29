1a8a19dbd39b90ade6dd240caed552ded4e8e76f
lamont <lamont.nelson@nyu.edu>
2016-04-28 22:44:50 -0400

    module to print process list

---
+++ pylib/ps.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def ps(hosts, grep_filter):
+    output = [] 
+    cmd = ['/bin/bash', '-c', "'ps -eLF | grep \"{}\"'".format(grep_filter)]
+    for host in hosts:
+        ssh_cmd = ['ssh', host]
+        ssh_cmd.extend(cmd)
+        try:
+            o = subprocess.check_output(ssh_cmd)
+            output.append(o)
+        except subprocess.CalledProcessError as e:
+            output.append("error calling ps! returncode {}".format(e.returncode))
+
+    return '\n'.join(output)
+
