b91fa4dd3e75abb49f12a602fcf12a34c70d7f00
lamont <lamont.nelson@nyu.edu>
2016-05-01 04:42:51 -0400

    bug

--- scripts/aws_experiments/extract_csv.sh
+++ scripts/aws_experiments/extract_csv.sh
@@ -1,11 +1,14 @@
 input_file=${1:-input.tgz}
 output_dir=${2:-$PWD}
 work_dir=${3:-~/.tmp}
+current_dir=$PWD
 mkdir -p $work_dir
+mkdir -p $output_dir
 cp $input_file $work_dir/target.tgz
 cd $work_dir
 tar --wildcards -xzvf target.tgz archive/*.csv
-mv archive/*.csv $output_dir/
+cd $current_dir
+mv $work_dir/archive/*.csv $output_dir/
 
 rm $work_dir/target.tgz
 rm -rf $work_dir/archive
