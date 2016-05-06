fdfab5102a3400007961d508a4ee20db3efda2bb
lamont <lamont.nelson@nyu.edu>
2016-05-05 21:19:47 -0400

    update multi dc aws script

--- scripts/aws_experiments/run_multi.sh
+++ scripts/aws_experiments/run_multi.sh
@@ -8,15 +8,27 @@ function new_experiment {
 duration=40
 prefix="multi_dc"
 
+exp_name=${prefix}_rw_fixed
+./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_50.yml -cc config/rw_fixed.yml -cc config/tpl_ww_paxos.yml -b rw_benchmark -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -c 128 -c 256 -c 384 -s 1 -r 3 -d $duration $exp_name 
+new_experiment $exp_name
+
+
 exp_name=${prefix}_rw
-./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_50.yml -cc config/rw.yml -cc config/tpl_ww_paxos.yml -b rw_benchmark -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -c 128 -c 256 -s 1 -r 3 -d $duration $exp_name 
+./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_50.yml -cc config/rw.yml -cc config/tpl_ww_paxos.yml -b rw_benchmark -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -c 128 -c 256 -c 384 -s 1 -r 3 -d $duration $exp_name 
 new_experiment $exp_name
 
-exp_name=${prefix}_tpcc
-./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_50.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -s 3 -r 3 -d $duration $exp_name 
+
+#exp_name=${prefix}_tpcc
+#./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_50.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -s 3 -r 3 -d $duration $exp_name 
+#new_experiment $exp_name
+
+
+exp_name=${prefix}_tpca_fixed
+./run_all.py -hh config/aws_hosts.yml -cc config/concurrent_50.yml -cc config/tpca_fixed.yml -cc config/tapir.yml -b tpca -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -s 3 -r 3 -d $duration $exp_name
 new_experiment $exp_name
 
-zipfs=( 0.0 0.5 1.0 )
+
+zipfs=( 0.0 0.25 0.5 0.75 1.0 )
 for zipf in "${zipfs[@]}"
 do
 	exp_name=${prefix}_tpca_zipf_${zipf}
