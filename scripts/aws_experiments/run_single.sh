88bed02141fe49bb21b80e554c860522d26ed801
lamont <lamont.nelson@nyu.edu>
2016-09-20 17:28:32 -0400

    uncomment commands

--- scripts/aws_experiments/run_single.sh
+++ scripts/aws_experiments/run_single.sh
@@ -8,17 +8,17 @@ function new_experiment {
 duration=90
 prefix="single_dc"
 
-# exp_name=${prefix}_zipf_graph
-# scripts/aws_experiments/zipf_graph.py $exp_name -c 64 -f config/tpca_zipf.yml config/tapir.yml config/concurrent_10.yml
-# new_experiment $exp_name
+exp_name=${prefix}_zipf_graph
+scripts/aws_experiments/zipf_graph.py $exp_name -c 64 -f config/tpca_zipf.yml config/tapir.yml config/concurrent_10.yml
+new_experiment $exp_name
 
-#exp_name=${prefix}_rw_fixed
-#./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/concurrent_10.yml -cc config/rw_fixed.yml -cc config/tpl_ww_paxos.yml -b rw_benchmark -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -s 1 -r 3 -d $duration $exp_name 
-#new_experiment $exp_name
+exp_name=${prefix}_rw_fixed
+./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/concurrent_10.yml -cc config/rw_fixed.yml -cc config/tpl_ww_paxos.yml -b rw_benchmark -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -s 1 -r 3 -d $duration $exp_name 
+new_experiment $exp_name
 
-# exp_name=${prefix}_rw
-# ./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/concurrent_10.yml -cc config/rw.yml -cc config/tpl_ww_paxos.yml -b rw_benchmark -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -s 1 -r 3 -d $duration $exp_name 
-# new_experiment $exp_name
+exp_name=${prefix}_rw
+./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/concurrent_10.yml -cc config/rw.yml -cc config/tpl_ww_paxos.yml -b rw_benchmark -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -s 1 -r 3 -d $duration $exp_name 
+new_experiment $exp_name
 
 exp_name=${prefix}_tpcc
 ./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/concurrent_10.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -s 10 -r 3 -d $duration $exp_name 
