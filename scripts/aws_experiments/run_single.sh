8b543e62c66e7cb42e873bca79d4725c3dcd8be0
lamont <lamont.nelson@nyu.edu>
2016-09-20 17:47:35 -0400

    update shard count

--- scripts/aws_experiments/run_single.sh
+++ scripts/aws_experiments/run_single.sh
@@ -25,14 +25,14 @@ exp_name=${prefix}_tpcc
 new_experiment $exp_name
 
 exp_name=${prefix}_tpca_fixed
-./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/client_closed.yml -cc config/concurrent_10.yml -cc config/tpca_fixed.yml -cc config/tapir.yml -b tpca -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -s 3 -r 3 -d $duration $exp_name
+./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/client_closed.yml -cc config/concurrent_10.yml -cc config/tpca_fixed.yml -cc config/tapir.yml -b tpca -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -s 6 -r 3 -d $duration $exp_name
 new_experiment $exp_name
 
 zipfs=( 0.0 0.25 0.5 0.75 1.0 )
 for zipf in "${zipfs[@]}"
 do
 	exp_name=${prefix}_tpca_zipf_${zipf}
-	./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/client_closed.yml -cc config/concurrent_10.yml -cc config/tpca_zipf.yml -cc config/tapir.yml -b tpca -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -z $zipf -s 3 -r 3 -d $duration $exp_name
+	./run_all.py -u 2 -hh config/aws_hosts.yml -cc config/client_closed.yml -cc config/concurrent_10.yml -cc config/tpca_zipf.yml -cc config/tapir.yml -b tpca -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -c 64 -z $zipf -s 6 -r 3 -d $duration $exp_name
 	new_experiment $exp_name
 done
 
