6991b7bf670714c79c8d759333d368778ec72fce
lamont <lamont.nelson@nyu.edu>
2016-09-26 11:30:46 -0400

    fix build

--- scripts/aws/build_aws.sh
+++ scripts/aws/build_aws.sh
@@ -4,6 +4,6 @@ instance_type=${3-m4.large}
 data_dir=${4-.ec2_data}
 
 fab --set data_dir="$data_dir" deploy_all:regions=${regions},servers_per_region=${region_servers},instance_type=${instance_type}
-sleep 5
-fab ec2.set_instance_roles deploy_continue
+sleep 30 
+fab --set data_dir="$data_dir" ec2.set_instance_roles deploy_continue
 
