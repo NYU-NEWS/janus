5a328cfb44782f5e43c750cbd534f40ee8ebc54b
lamont <lamont.nelson@nyu.edu>
2016-09-26 17:21:03 -0400

    deploy multi script

--- scripts/aws/deploy_multi.sh
+++ scripts/aws/deploy_multi.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-fab --set data_dir=.ec2_multi deploy_all:regions=us-west-2:eu-west-1:ap-northeast-2,servers_per_region=13:12:12,instance_type=t2.small
+data_dir=.ec2_multi
+
+regions=us-west-2:eu-west-1:ap-northeast-2
+servers=7:6:6
+
+$janus/scripts/aws/build_aws.sh "$regions" "$servers" m4.large "$data_dir"
