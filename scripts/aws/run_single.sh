2e9ba872273ce5275f886f35d3befa32ddb139c8
lamont <lamont.nelson@nyu.edu>
2016-09-27 12:45:22 -0400

    tpcc clients for single

--- scripts/aws/run_single.sh
+++ scripts/aws/run_single.sh
@@ -26,8 +26,10 @@ function new_experiment {
 	rm -rf tmp/* log/* 
 	tar -czvf ~/${1}.tgz archive && rm -rf archive && mkdir -p archive
 	printf '=%.0s' {1..40}
+	echo
 	echo "end $1"
 	printf '=%.0s' {1..40}
+	echo
 }
 
 function zipf_graph {
@@ -59,7 +61,7 @@ function rw {
 function tpcc {
 	shards=$1
 	exp_name=${prefix}_tpcc_${shards}
-	./run_all.py -g -u 2 -hh config/aws_hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 18 -s $shards -r 3 -d $duration $exp_name 
+	./run_all.py -g -u 2 -hh config/aws_hosts.yml -cc config/client_closed.yml -cc /tmp/concurrent.yml -cc config/tpcc.yml -cc config/tapir.yml -b tpcc -m brq:brq -m 2pl_ww:multi_paxos -m occ:multi_paxos -m tapir:tapir -c 1 -c 2 -c 4 -c 8 -c 16 -c 32 -s $shards -r 3 -d $duration $exp_name
 	new_experiment $exp_name
 }
 
