0c96fd22a22ec1e9a1e5579c44427b0f4396fa32
lamont <lamont.nelson@nyu.edu>
2016-10-27 15:47:25 -0400

    cleanup deptran/occ dir

--- deptran/occ/sched.h
+++ deptran/occ/sched.h
@@ -1,7 +1,3 @@
-//
-// Created by shuai on 11/25/15.
-//
-
 #pragma once
 
 #include "../classic/sched.h"
@@ -12,7 +8,6 @@ class OCCSched: public ClassicSched {
  public:
   OCCSched();
   virtual mdb::Txn *get_mdb_txn(const i64 tid);
-//  virtual mdb::Txn *get_mdb_txn(const RequestHeader &req);
 };
 
 } // namespace rococo
