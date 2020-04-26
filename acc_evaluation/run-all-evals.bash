50f4663a612cabad881615d122e12f4990845e5f
Haonan <haonanlu@usc.edu>
2020-04-26 13:19:03 -0700

    nits

--- acc_evaluation/run-all-evals.bash
+++ acc_evaluation/run-all-evals.bash
@@ -9,10 +9,10 @@ expt_dir_base="${log_dir}/${expt_id}"
 mkdir -p ${expt_dir_base}
 
 for bench in ${benchmarks}; do  # run all benchmarks
-    bench_dir="${expt_dir_base}/bench"
+    bench_dir="${expt_dir_base}/${bench}"
     mkdir -p ${bench_dir}
     for mode in ${modes}; do    # each bench runs all modes
-        mode_dir="${bench_dir}/mode"
+        mode_dir="${bench_dir}/${mode}"
         mkdir -p ${mode_dir}
         for n_concurrent in ${n_concurrents}; do   # each mode varies n_concurrent
             n_concurrent_dir="${mode_dir}/n_concurrent_${n_concurrent}"
